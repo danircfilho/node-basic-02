e8a17ad577e4657f9661f243539337aab6dc59d8
danircfilho <danircfilho@hotmail.com>
2021-12-17 11:37:57 -0300

    first-commit

---
+++ db/conn.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysql',
+})
+
+console.log('Conectado ao Banco de Dados MySQL!')
+
+module.exports = pool
---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const pool = require('./db/conn')
+
+const app = express() 
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine()) 
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const sqlbd = `INSERT INTO books (??, ??) VALUES (?, ?)` //substituir (coluna 2 ??, linha)
+  const data = ['title', 'pageqty', title, pageqty] //substituir o item (title, pageqty) com as variáveis (title, pageqty)
+
+  pool.query(sqlbd, data, function (err) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    res.redirect('/books')
+  })
+})
+
+app.get('/books', (req, res) => {
+
+  const sqlbd = "SELECT * FROM books"
+
+  pool.query(sqlbd, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    const books = data
+    res.render('books', { books })
+  })
+})
+
+//READ - rota buscar livros individualmente (get) - WHERE - SQL
+app.get('/books/:id', (req, res) => {
+
+  const id = req.params.id
+
+  const sqlbd = `SELECT * FROM books WHERE ?? = ?`
+  const data = ['id', id]
+
+  pool.query(sqlbd, data, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const bookind = data[0]    
+    res.render('bookind', { bookind }) 
+  })
+})
+
+app.get('/books/edit/:id', (req, res) => {
+
+  const id = req.params.id
+  const sqlbd = `SELECT * FROM books WHERE ?? = ?`
+  const data = ['id', id]
+
+  pool.query(sqlbd, data, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const bookind = data[0]    
+    res.render('editbookind', { bookind }) 
+  })
+})
+
+app.post('/books/updatebook', (req, res) => {
+
+  const id = req.body.id
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const sqlbd = `UPDATE books SET ?? = ?, ?? = ? WHERE ?? = ?`
+  const data = ['title', title, 'pageqty', pageqty, 'id', id]
+
+  pool.query(sqlbd, data, function (err) {
+    if (err) {
+      console.log(err)
+      return
+    }    
+    res.redirect('/books') 
+  })
+})
+
+app.post('/books/remove/:id', (req, res) => {
+
+  const id = req.params.id
+
+  const sqlbd = `DELETE FROM books WHERE ?? = ?` 
+  const data = ['id', id]
+
+  pool.query(sqlbd, data, function (err) {
+    if (err) {
+      console.log(err) 
+    }
+    res.redirect('/books') 
+  })
+})
+
+app.listen(3000)
+
